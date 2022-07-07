a6911163643ffeee4c3f36694dc086f6c7647b9f
kosuke-ando <105697560+kosuke-ando@users.noreply.github.com>
2022-07-07 15:18:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,54 +3,6 @@
 
 console.log(Math.floor(Math.random() * 10 + 1));
 
-// function getNUmber(number) {
-//   const randomNumber = Math.floor(Math.random() * 10 + 1);
-//   if (typeof number === "number") {
-//     if (number === randomNumber) {
-//       console.log(randomNumber);
-//       return "すみっこオールスター";                    // すみっこオールスター　表示
-//     } else if (number > randomNumber) {
-//       console.log(randomNumber);
-//       return "ねこ";                    // ねこ　表示
-//     } else if (number < randomNumber) {
-//       console.log(randomNumber);
-//       return "とかげ";                    // とかげ　表示
-//     } else {
-//       console.log(randomNumber);
-//       return "たぴおか";                    // たぴおか　を表示
-//     }
-//   } else if (typeof number === "string") {
-//     if (number === "とかげ") {
-//       return "とかげ";                    // とかげ　を表示
-//     } else if (number === "ねこ") {
-//       return "ねこ";                      // ねこ　を表示
-//     } else if (number === "しろくま") {
-//       return "しろくま";                  // しろくま　を表示
-//     } else if (number === "ぺんぎん") {
-//       return "ぺんぎん";                  // ぺんぎん　を表示
-//     } else if (number === "とんかつ") {
-//       return "とんかつ";                  // とんかつ　を表示
-//     }else{
-//       return "ぶらっくたぴおか";          // ぶらっくたぴおか　を表示
-//     }
-//   }else{
-//     return "ダースベーダー";                    // ダースベーダー　を表示
-//   }
-// }
-
-// console.log("0",(getNUmber(0)));
-// console.log("1",(getNUmber(1)));
-// console.log("3",(getNUmber(3)));
-// console.log("5",(getNUmber(5)));
-// console.log("8",(getNUmber(8)));
-// console.log("10",(getNUmber(10)));
-// console.log("12345　固定",(getNUmber("12345")));
-// console.log("true　固定",(getNUmber(true)));
-// console.log("undefined　固定",(getNUmber(undefined)));
-// console.log("とかげ　固定",(getNUmber("とかげ")));
-// console.log("トカゲ　固定",(getNUmber("トカゲ")));
-
-
 const input = document.getElementById("input");
 const output = document.getElementById("output");
 
@@ -61,16 +13,16 @@ function getNumber() {
    if (typeof inputNumber === "number") {
       if (inputNumber === randomNumber) {
          output.innerHTML = "すみっこオールスター";                    // すみっこオールスター　表示
-         document.getElementById("outputImage").innerHTML = "<img src='allstar.png'>";
+         document.getElementById("outputImage").innerHTML = "<img src='https://user-images.githubusercontent.com/105697560/177699259-a394ade7-8652-457a-b149-da8c4ba99ce9.png'>";
       } else if (inputNumber > randomNumber) {
          output.innerHTML = "ねこ";                    // ねこ　表示
-         document.getElementById("outputImage").innerHTML = "<img src='neko.png'>";
+         document.getElementById("outputImage").innerHTML = "<img src='https://user-images.githubusercontent.com/105697560/177699267-f9d4ca7e-c9eb-488c-9170-fe853424dbd8.png'>";
       } else if (inputNumber < randomNumber) {
          output.innerHTML = "とかげ";                    // とかげ　表示
-         document.getElementById("outputImage").innerHTML = "<img src='tokage.png'>";
+         document.getElementById("outputImage").innerHTML = "<img src='https://user-images.githubusercontent.com/105697560/177699275-d20fba5a-fad8-4c74-8515-623978e121da.png'>";
       } else {
          output.innerHTML = "たぴおか";                    // たぴおか　を表示
-         document.getElementById("outputImage").innerHTML = "<img src='tapioka.png'>";
+         document.getElementById("outputImage").innerHTML = "<img src='https://user-images.githubusercontent.com/105697560/177699271-6a6d6b90-2541-4788-9119-1ce41e529a87.png'>";
       }
    } else if (typeof inputNumber === "string") {
       if (inputNumber === "とかげ") {
